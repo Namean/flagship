9c30c89bc53236a4e42183e2eb67e3d4e74fa41f
dev-capitan <dwilson@capitanappliance.com>
2024-12-30 20:26:14 -0500

    added List component

--- src/App.js
+++ src/App.js
@@ -10,12 +10,19 @@ import { FlexContainer } from "./components/FlexContainer.component";
 import { Footer } from "./components/Footer.component";
 import { Form, FormInput } from "./components/Form.component";
 import { StaticTable, DynamicTable } from "./components/table/Table.component";
-import ShoppingList from "./components/list/ShoppingList.component";
+import ShoppingList from "./components/lists/ShoppingList.component";
 import { companyData, w3data } from "./data/table.data";
+import { List } from "./components/lists/List.component";
+
+function ListElement({ props }) {
+  return <li>{props.data}</li>;
+}
 
 function App() {
   return (
     <div className="App">
+      <List props={{ data: [1, 2, 3], component: ListElement }} />
+      {/* <ListElement props={{ data: "Hello World!" }} /> */}
       <Header />
 
       {/* <ShoppingList /> */}
---
+++ src/components/lists/List.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+/*
+const Label = props => <span>{props.children}</span>
+const Button = props => {
+    const Inner = props.inner; // Note: variable name _must_ start with a capital letter 
+    return <button><Inner>Foo</Inner></button>
+}
+const Page = () => <Button inner={Label}/>
+*/
+export const List = ({ props }) => {
+  const Component = props.component; // Note: variable name _must_ start with a capital letter
+  //   return props.data.map((v) => <Component>{v}</Component>);
+  let data = [1, 2, 3];
+  return props.data.map((v) => <Component props={{ data: v }} />);
+};
